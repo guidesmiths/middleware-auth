66fd9b4c8ae6779239d14f8615927f5c68e1e64d
Daniel Herrero <danielherrerohernando@gmail.com>
2020-09-04 12:49:35 +0200

    feat: adding basic code
    ...
    
    noticket

---
+++ index.js
@@ -0,0 +1,33 @@
+const { get } = require('axios');
+
+module.exports = () => {
+	const start = async ({ config }) => {
+
+    const { url } = config;
+
+    if (!url) {
+      throw new Error('Auth API url is not set, please check configuration.');
+    }
+
+    const getUserData = token => get(url)
+      .then(response => response.data)
+      .catch(err => err);
+
+		const authenticate = async (req, res, next) => {
+			try {
+				const token = req.headers.authorization;
+				if (!token) throw new Error('Missing token');
+				res.locals.userData = await getUserData(token);
+				return next();
+			} catch (error) {
+				return res.status(404).send(`Authentication failed: ${error.message}`);
+			}
+		};
+
+		return {
+			authenticate,
+		};
+	};
+
+	return { start };
+};
